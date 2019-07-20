e010aa7858976f688deb39a6ca26add73c6c12a8
limoniumstatice <strangeholic@gmail.com>
2019-07-20 13:34:42 +0000

    marlin/sailfish: always use highest clang version
    Change-Id: I3077e274319d97f78c587b977f1d18c550e917fa

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,6 +6,13 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)
+
+-include vendor/google/marlin/BoardConfigVendor.mk
