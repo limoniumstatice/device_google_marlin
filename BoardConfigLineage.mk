b55fa40fe9ac3296e4d15d243bbd3a9e3b86c73e
limoniumstatice <strangeholic@gmail.com>
2019-10-10 09:31:10 +0000

    marlin: add fini defconfig
    Change-Id: I593a4881fb6ca8a2eb558a7e651830aa3de9d865

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -7,7 +7,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
+TARGET_KERNEL_CONFIG := fini_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 
 BUILD_BROKEN_DUP_RULES := true
